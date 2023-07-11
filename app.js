0076fdb70d3aa038832e7ab4217f1ba153cbe24a
gondr99 <gondr99@gmail.com>
2023-07-11 15:38:58 +0900

    afterschool

--- app.js
+++ app.js
@@ -1,19 +1,141 @@
 window.onload = ()=>{
     const lectureList = 
     [
-        {name:"2D Parkour Runner 게임 개발반", file:"01.png", difficulty:"중급", teacher: "이재희", grade:[1,2], desc:"파쿠르 러너 게임을 처음부터 끝까지 만들어보는 수업"},
-        {name:"유니티 뱀서라이크 게임개발반", file:"03.png", difficulty:"상급", teacher: "채한승", grade:[1], desc:"뱀파이어 서바이버 게임 클로닝과 출시, 광고삽입, 앱스토어별 규정 학습"},
-        {name:"유니티 왕기초 어몽어스구현반", file:"04.png", difficulty:"초급", teacher: "김인옥", grade:[1,2], desc:"어몽어스 게임속에 나오는 미션 미니게임들을 유니티로 구현해보는 반"},
-        {name:"길건너 친구들 클론닝을 통해 유니티 기초 배우기", file:"08.png", difficulty:"초급", teacher: "이준호", grade:[1, 2], desc:"길건너 친구들 게임을 클론닝하면서 게임개발의 기초학습을 배움"},
-        {name:"유니티 최적화 기법 프로그래밍반", file:"05.png", difficulty:"상급", teacher: "유준무", grade:[2], desc:"유니티 최적화 기법, 딥 프로파일링, Gc, 힙익스플로러를 학습하고 유니티 신규 시스템인 Job과 DOTS의 개념도 익힌다."},
-        {name:"비동기 프로그래밍&게임서버 제작반", file:"06.png", difficulty:"중급", teacher: "최홍석", grade:[1,2], desc:"C# 비동기 프로그래밍 기법을 이용한 서버와 유니티 클라이언트를 활용하여 멀티플레이어 세균전 게임 만들기"},
-        {name:"유니티 ScriptableObject 프로그래밍반", file:"02.png", difficulty:"중급", teacher: "이상진", grade:[2], desc:"Scriptable Object의 이해와 이를 활용한 Skinned Mesh 장비교체 알고리즘 개발"},
-        {name:"유니티 CG, HLSL 셰이더 프로그래밍반", file:"07.png", difficulty:"중급", teacher: "최선한", grade:[1, 2], desc:"유니티 CG 프로그래밍 기법과 URP변환, HLSL 프로그래밍, 라이트 셰이더 등을 학습"},
-        
-        {name:"3D 게임 그래픽 기초(1학년)", file:"09.png", difficulty:"초급", teacher: "최용준", grade:[1], desc:"게임에서 사용하는 이미지의 이해와, 노말, 라이팅, PBR에 대한 이해"},
-        {name:"3D 게임 그래픽 심화(2학년)", file:"10.png", difficulty:"중급", teacher: "김종엽", grade:[2], desc:"PBR텍스쳐, 서브스턴스 디자이너, 텍스쳐 제작 등을 학습"},
-        
-        {name:"게임 기획자가 알아야 할 게임그래픽스와 게임메커니즘의 이해", file:"11.png", difficulty:"초,중급", teacher: "정직한", grade:[1, 2], desc:"게임기획의 기초지식 학습"},
+        {
+            name:"3매치 퍼즐게임 개발반(with Unity)", 
+            file:"01.png", 
+            difficulty:"중급", 
+            teacher: "이재희", 
+            grade:[1], 
+            desc:"3매치 퍼즐 게임을 개발하는 중급 유니티반",
+            part:2,
+        },
+        {
+            name:"C++ 프로그래밍반(초급)", 
+            file:"02.png", 
+            difficulty:"초급", 
+            teacher: "이혜령", 
+            grade:[1], 
+            desc:"c++ 기초 문법과 텍스트 기반 게임을 만들어보는 반",
+            part:2,
+        },
+        {
+            name:"뱀서라이크 게임 심화편", 
+            file:"03.png", 
+            difficulty:"중급", 
+            teacher: "채한승(외부강사)", 
+            grade:[1], 
+            desc:"뱀서라이크 게임에 시네머신을 활용한 카메라 무빙 학습",
+            part:1,
+        },
+        {
+            name:"유니티 VR게임 개발반", 
+            file:"04.png", 
+            difficulty:"중급", 
+            teacher: "표재석", 
+            grade:[1], 
+            desc:"구글 카드보드를 활용한 VR게임과 포톤 네트워크를 활용한 멀티플레이어 VR게임 개발",
+            part:2,
+        },
+        {
+            name:"유니티 타워디펜스", 
+            file:"05.png", 
+            difficulty:"초급", 
+            teacher: "박서린", 
+            grade:[1], 
+            desc:"코루틴, 싱글톤, SO를 활용한 타워디펜스 게임 개발",
+            part:2,
+        },
+        {
+            name:"유니티 로그라이크 게임 개발반", 
+            file:"06.png", 
+            difficulty:"초급", 
+            teacher: "유다율", 
+            grade:[1], 
+            desc:"로그라이크 게임 만들고 퍼블리싱하기",
+            part:2,
+        },
+        {
+            name:"유니티 2D 디펜스 게임", 
+            file:"07_no.png", 
+            difficulty:"초급", 
+            teacher: "이명희", 
+            grade:[2], 
+            desc:"URP에서 만드는 2D 타워디펜스 게임 개발반",
+            part:1,
+        },
+        {
+            name:"언리얼 게임개발반", 
+            file:"08.png", 
+            difficulty:"고급", 
+            teacher: "송윤지", 
+            grade:[2], 
+            desc:"블루프린트부터 C++까지 모두 사용한 언리얼 게임 개발학습",
+            part:1,
+        },
+        {
+            name:"유니티 심화반(외부강사)", 
+            file:"09_no.png", 
+            difficulty:"고급", 
+            teacher: "유준무", 
+            grade:[2], 
+            desc:"아직 외부강사의 정보가 없습니다.",
+            part:1,
+        },
+        {
+            name:"C++ SFML 라이브러리를 활용한 게임 프레임워크 개발", 
+            file:"10.png", 
+            difficulty:"중급", 
+            teacher: "이준용", 
+            grade:[2], 
+            desc:"SFML 라이브러리를 이용한 멀티 플랫폼 게임 개발",
+            part:1,
+        },
+        {
+            name:"HTML5 웹 및 게임개발반", 
+            file:"11.png", 
+            difficulty:"중급", 
+            teacher: "최선한", 
+            grade:[2], 
+            desc:"Node.js, TypeScript, React를 이용한 웹앱 및 Phaser엔진을 활용한 웹게임 개발",
+            part:1,
+        },
+        {
+            name:"유니티 심화반", 
+            file:"12_no.png", 
+            difficulty:"중급", 
+            teacher: "이상진", 
+            grade:[2], 
+            desc:"아직 외부강사님 정보가 없습니다.",
+            part:1,
+        },
+        {
+            name:"게임기획 심화", 
+            file:"13_no.png", 
+            difficulty:"중급", 
+            teacher: "외부기획선생님", 
+            grade:[2], 
+            desc:"아직 외부강사님 정보가 없습니다.",
+            part:1,
+        },
+        {
+            name:"게임아트 기초반", 
+            file:"14.png", 
+            difficulty:"초급", 
+            teacher: "이수진, 현소연", 
+            grade:[1], 
+            desc:"손맵 그리기",
+            part:1,
+        },
+        {
+            name:"게임아트 심화반", 
+            file:"15_no.png",
+            difficulty:"중급", 
+            teacher: "외부 아트선생님", 
+            grade:[2], 
+            desc:"아직 외부강사님 정보가 없습니다.",
+            part:1,
+        },
     ];
     const popup = document.querySelector("#popup");
     const image = document.querySelector("#descImg");
@@ -37,7 +159,7 @@ window.onload = ()=>{
     });
 }    
 
-function makeTemplate({name, difficulty, teacher, grade, desc, file})
+function makeTemplate({name, difficulty, teacher, grade, desc, file, part})
 {
     const div = document.createElement("div");
     div.innerHTML = `
@@ -49,6 +171,7 @@ function makeTemplate({name, difficulty, teacher, grade, desc, file})
             <div class="info-box">
                 <div class="difficulty">${difficulty}</div>
                 <div class="grade">${makeGradeBadge(grade)}</div>
+                <div class="part">${part}회</div>
             </div>
             <div class="desc">${desc}</div>
         </div>
